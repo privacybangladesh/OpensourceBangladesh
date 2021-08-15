5705912c63c6176a6fcbac1668af6dc4fc720e96
Md. Abu Sayed <67384370+opensourcebangladesh@users.noreply.github.com>
2021-08-15 12:09:42 +0600

    Font Changed

--- assets/js/mermaid.js
+++ assets/js/mermaid.js
@@ -2,7 +2,7 @@ import mermaid from 'mermaid/dist/mermaid';
 
 var config = {
   theme: 'default',
-  fontFamily: '"Jost", -apple-system, blinkmacsystemfont, "Segoe UI", roboto, "Helvetica Neue", arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
+  fontFamily: '"Hind Siliguri", -apple-system, blinkmacsystemfont, "Segoe UI", roboto, "Helvetica Neue", arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
 };
 
 document.addEventListener('DOMContentLoaded', () => {
--- content/_index.md
+++ content/_index.md
@@ -1,7 +1,7 @@
 ---
 title : "ওপেনসোর্স বাংলাদেশ"
-description: "Doks is a Hugo theme helping you build modern documentation websites that are secure, fast, and SEO-ready — by default."
-lead: "Doks is a Hugo theme helping you build modern documentation websites that are secure, fast, and SEO-ready — by default."
+description: "বাংলা ভাষা। আমার ভাষা।"
+lead: "বাংলা ভাষা। আমার ভাষা।"
 date: 2020-10-06T08:47:36+00:00
 lastmod: 2020-10-06T08:47:36+00:00
 draft: false
